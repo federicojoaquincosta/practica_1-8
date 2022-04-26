e237b296f2001f90614902d04ea24bff2fbc6ed7
Federico Costa <federicojoaquincosta@gmail.com>
2022-04-26 11:35:26 -0300

    estructura if

---
+++ algoritmoif.js
@@ -0,0 +1,13 @@
+var numeroIngresado = prompt("Ingrese un número", "0");
+
+if (numeroIngresado == 3) {
+    document.write(`
+    <h2>si es igual</h2>
+    `)
+}
+else {
+    document.write(`
+            <h2>no es igual</h2>
+            `)
+}
+
